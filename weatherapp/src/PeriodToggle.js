41a4393cf688def3c3f2b84f98df5cf20869d532
webfolly <zhanghuadong2015@gmail.com>
2018-02-02 22:10:28 +0800

    optimized forecast period toggle function

--- weatherapp/src/PeriodToggle.js
+++ weatherapp/src/PeriodToggle.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default function PeriodToggle (props) {
     let period = props.period;
-    //console.log(typeof props.period);
     return (
         <form action="#" className="period-toggle">
             <input type="radio" name="period" value={'3'}  onChange={ (e) => props.onChange(e.target.name,e.target.value) } checked={period==="3" ? true:false}/> 3-Day Forecast
